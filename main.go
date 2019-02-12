22d5384ff59851c662176826baf7545c49bc3916
Raggaer <nakotoffana@gmail.com>
2019-02-12 23:08:45 +0100

    Add raw and server flags

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/atotto/clipboard"
 )
 
+var retrieveRaw bool
+
 func main() {
+	// Parse flags
+	flag.StringVar(&serverURL, "server", "https://hastebin.com", "Hastebin server URL")
+	flag.BoolVar(&retrieveRaw, "raw", false, "Retrieve raw URL")
+	flag.Parse()
+
 	info, err := os.Stdin.Stat()
 	if err != nil {
 		panic(err)
@@ -20,11 +29,11 @@ func main() {
 	// Or if input should be an argument file
 	if info.Mode()&os.ModeCharDevice != 0 || info.Size() <= 0 {
 		// Retrieve file to read from command line arguments
-		if len(os.Args) <= 1 {
+		if len(flag.Args()) <= 0 {
 			fmt.Println("Specify a file to read or pipe data to this application")
 			return
 		}
-		fileName := os.Args[1]
+		fileName := flag.Args()[0]
 		content, err = ioutil.ReadFile(fileName)
 	} else {
 		// Read everything from stdin
@@ -50,13 +59,19 @@ func main() {
 	}
 
 	fmt.Println("Document uploaded to " + serverURL)
+	finalURL := serverURL + "/" + resp.Key
+	if retrieveRaw {
+		finalURL = serverURL + "/raw/" + resp.Key
+	}
+
+	log.Println(retrieveRaw)
 
 	// Try to write result to clipboard if possible
-	if err := clipboard.WriteAll(serverURL + "/" + resp.Key); err != nil {
-		fmt.Println("- " + serverURL + "/" + resp.Key)
+	if err := clipboard.WriteAll(finalURL); err != nil {
+		fmt.Println("- " + finalURL)
 		fmt.Println("Unable to copy link to clipboard")
 		fmt.Println(err.Error())
 	} else {
-		fmt.Println("- " + serverURL + "/" + resp.Key + "(copied to clipboard)")
+		fmt.Println("- " + finalURL + " (copied to clipboard)")
 	}
 }
